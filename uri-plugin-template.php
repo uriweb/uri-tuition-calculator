96b96a81a97015351d62117400c7050a12214e24
alexandragauss <alexandra_gauss@uri.edu>
2023-08-11 10:28:59 -0400

    Change plugin name to uri-courses-calculator

--- uri-plugin-template.php
+++ uri-plugin-template.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name: URI Plugin Template
+ * Plugin Name: URI Courses Calculator
  * Plugin URI: http://www.uri.edu
- * Description: A template for plugins
+ * Description: A calculator for courses and fees
  * Version: 0.1.0
  * Author: URI Web Communications
  * Author URI: https://today.uri.edu/
  *
  * @author: Brandon Fuller <bjcfuller@uri.edu>
- * @package uri-plugin-template
+ * @author: Alexandra Gauss <alexandra_gauss@uri.edu>
+ * @package uri-courses-calculator
  */
 
 // Block direct requests
@@ -19,13 +20,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Include css and js
  */
-function uri_plugin_template_enqueues() {
+function uri_courses_calculator_enqueues() {
 
-	wp_register_style( 'uri-plugin-template-css', plugins_url( '/css/style.built.css', __FILE__ ) );
-	wp_enqueue_style( 'uri-plugin-template-css' );
+	wp_register_style( 'uri-courses-calculator-css', plugins_url( '/css/style.built.css', __FILE__ ) );
+	wp_enqueue_style( 'uri-courses-calculator-css' );
 
-	wp_register_script( 'uri-plugin-template-js', plugins_url( '/js/script.built.js', __FILE__ ) );
-	wp_enqueue_script( 'uri-plugin-template-js' );
+	wp_register_script( 'uri-courses-calculator-js', plugins_url( '/js/script.built.js', __FILE__ ) );
+	wp_enqueue_script( 'uri-courses-calculator-js' );
 
 }
-add_action( 'wp_enqueue_scripts', 'uri_plugin_template_enqueues' );
+add_action( 'wp_enqueue_scripts', 'uri_courses_calculator_enqueues' );
