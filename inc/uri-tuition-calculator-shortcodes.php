b7878ec5ef260832e34d6bb422a33684835bcab8
alexandragauss <alexandra_gauss@uri.edu>
2023-09-12 13:34:39 -0400

    Normalize attributes keys, lowercase

--- inc/uri-tuition-calculator-shortcodes.php
+++ inc/uri-tuition-calculator-shortcodes.php
@@ -9,7 +9,10 @@
   * Create a shortcode for displaying calculator.
   */
 function uri_tuition_calculator_shortcode( $attributes ) {
+	// normalize attribute keys, lowercase
+	$attributes = array_change_key_case( (array) $attributes, CASE_LOWER );
 
+	// default attributes
 	$attributes = shortcode_atts(
 		 array(
 			 'title' => 'Tuition & Fees Calculator',
