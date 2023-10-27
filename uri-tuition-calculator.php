59f04768718614779e588229789e86ca6e1e480a
alexandragauss <alexandra_gauss@uri.edu>
2023-10-27 12:00:57 -0400

    Add cache buster

--- uri-tuition-calculator.php
+++ uri-tuition-calculator.php
@@ -21,6 +21,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'URI_TUITION_CALCULATOR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'URI_TUITION_CALCULATOR_URL', str_replace( '/assets', '/', plugins_url( 'assets', __FILE__ ) ) );
 
+/**
+ * Returns version from package.json to be used for cache busting
+ *
+ * @return str
+ */
+function uri_tuition_calculator_cache_buster() {
+	static $cache_buster;
+	if ( empty( $cache_buster ) && function_exists( 'get_plugin_data' ) ) {
+		$values = get_plugin_data( URI_TUITION_CALCULATOR_PATH . 'uri-tuition-calculator.php', false );
+		$cache_buster = $values['Version'];
+	} else {
+		$cache_buster = gmdate( 'Ymd', strtotime( 'now' ) );
+	}
+	return $cache_buster;
+}
+
 // Include the admin settings screen
 include_once( URI_TUITION_CALCULATOR_PATH . 'inc/uri-tuition-calculator-settings.php' );
 
@@ -31,10 +47,10 @@ include_once( URI_TUITION_CALCULATOR_PATH . 'inc/uri-tuition-calculator-settings
  */
 function uri_tuition_calculator_enqueues() {
 
-	wp_register_style( 'uri-tuition-calculator-css', plugins_url( '/css/style.built.css', __FILE__ ) );
+	wp_register_style( 'uri-tuition-calculator-css', plugins_url( '/css/style.built.css', __FILE__ ), array(), uri_tuition_calculator_cache_buster(), 'all' );
 	wp_enqueue_style( 'uri-tuition-calculator-css' );
 
-	wp_register_script( 'uri-tuition-calculator-js', plugins_url( '/js/script.built.js', __FILE__ ) );
+	wp_register_script( 'uri-tuition-calculator-js', plugins_url( '/js/script.built.js', __FILE__ ), array(), uri_tuition_calculator_cache_buster(), true );
 	wp_enqueue_script( 'uri-tuition-calculator-js' );
 
 	$spreadsheet = get_option( 'uri_tuition_calculator_spreadsheet' );
