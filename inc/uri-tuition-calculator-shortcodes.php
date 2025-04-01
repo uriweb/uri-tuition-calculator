08661a7856638a6692e55db31b5d78b83310f8cc
alexandragauss <alexandra_gauss@uri.edu>
2025-04-01 15:25:27 -0400

    Add attributes to shortcode

--- inc/uri-tuition-calculator-shortcodes.php
+++ inc/uri-tuition-calculator-shortcodes.php
@@ -17,20 +17,85 @@ function uri_tuition_calculator_shortcode($attributes)
 	// default attributes
 	$attributes = shortcode_atts(
 		array(
-			'title' => 'Tuition & Fees Calculator',
+			'calculator_name' => null, // Takes 'summer', 'jterm', 'pellgrant' 
+			'title' => 'Tuition and Fees Calculator',
+			'spreadsheet' => null, // URL of sheet Ex. 'https://docs.google.com/spreadsheets/d/[spreadsheetID]/gviz/tq?tqx=out:csv&sheet=[SheetName]'
+			'semester' => null, // ex. "Spring 2025"
 		),
 		$attributes,
 	);
 
-	ob_start();
-	include 'uri-tuition-calculator-display.php';
+	// If summer calculator 
+	if( $attributes['calculator'] == 'summer') {
+		$spreadsheet = $attributes['spreadsheet'];
+		$semester = $attributes['semester'];
+	//localize scripts 
+	wp_localize_script(
+		'uri-tuition-calculator-js',
+	   'summer',
+	   array(
+		   'spreadsheet' => $spreadsheet,
+		   'semester' => $semester
+	   )
+	   );
+
+	   //Include display 
+	   ob_start();
+	include 'utc-display-summer.php';
 	return ob_get_clean();
+	}
+
+
+	// If jterm calculator 
+	if( $attributes['calculator'] == 'jterm') {
+		//localize scripts 
+		$spreadsheet = $attributes['spreadsheet'];
+		$semester = $attributes['semester'];
+	//localize scripts 
+	wp_localize_script(
+		'uri-tuition-calculator-js',
+	   'jterm',
+	   array(
+		   'spreadsheet' => $spreadsheet,
+		   'semester' => $semester
+	   )
+	   );
+
+		//Include display
+	ob_start();
+	include 'utc-display-jterm.php';
+	return ob_get_clean();
+	}
+
+	// If pellgrant calculator
+	if( $attributes['calculator'] == 'pellgrant') {
+		$attributes['title'] == 'Summer Federal Pell Grant Estimator';
+		$spreadsheet = $attributes['spreadsheet'];
+		$semester = $attributes['semester'];
+	//localize scripts 
+	wp_localize_script(
+		'uri-tuition-calculator-js',
+	   'pellgrant',
+	   array(
+		   'spreadsheet' => $spreadsheet,
+		   'semester' => $semester
+	   )
+	   );
+
+// Include display 
+ob_start();
+	include 'utc-display-pell-grant.php';
+	return ob_get_clean();
+	}
+
+	
 }
 
 add_shortcode('tuition-calculator', 'uri_tuition_calculator_shortcode');
 
 
 // Pell Grant Calculator Shortcode
+/*
 function uri_tuition_calculator_pell_grant_shortcode($attributes)
 {
 	// normalize attribute keys, lowercase
@@ -41,6 +106,7 @@ function uri_tuition_calculator_pell_grant_shortcode($attributes)
 		array(
 			'title' => 'Summer Federal Pell Grant Estimator',
 			'spreadsheet' => null, //URL of sheet Ex. 'https://docs.google.com/spreadsheets/d/[spreadsheetID]/gviz/tq?tqx=out:csv&sheet=[SheetName]'
+			'semester' => "Summer", // Ex 'Summer 2025'
 		),
 		$attributes,
 	);
@@ -65,3 +131,4 @@ function uri_tuition_calculator_pell_grant_shortcode($attributes)
 }
 
 add_shortcode('pell-grant-calculator', 'uri_tuition_calculator_pell_grant_shortcode');
+*/
