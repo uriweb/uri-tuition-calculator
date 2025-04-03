7e42dc729688874e29cd391af4d6e3761dd49555
alexandragauss <alexandra_gauss@uri.edu>
2025-04-03 10:52:27 -0400

    Add courses in dropdown etc

--- src/js/tc-pellgrant.js
+++ src/js/tc-pellgrant.js
@@ -1,39 +1,97 @@
+/* eslint-disable no-undef */
 /**
- * SCRIPTS
- *
- * @package uri-tuition-calculator 
+ * Pell Grant Calculator
  */
-
 
 ( function() {
 	'use strict';
 
-	window.addEventListener( 'load', function() {
-		creditDropdown();
-        awardDropdown();
+	/*
+	document.getElementById( 'pell-grant-calc' ).addEventListener( 'load', function() {
+		uriPellGrantInit();
 	} );
-     
+     */
 
-function creditDropdown() {
-    const awardOptions = document.getElementById("c-amount");
+	window.addEventListener( 'load', function() {
+		if ( document.getElementById( 'pell-grant-calc' ) ) {
+			uriPellGrantInit();
+		}
+	} );
 
-    for (let i = 1; i <= 12; i++) {
-        const option = document.createElement("option");
-    option.value = i;
-    option.text = i;
-    awardOptions.add(option);
-    }
-}
+	function uriPellGrantInit() {
+		const urlSpreadsheet = spreadsheet.url;
 
-function awardDropdown() {
-    const awardOptions = document.getElementById("p-award");
+		parseDataPellGrant( urlSpreadsheet, dropDownsPellGrant );
 
-    for (let i = 370; i <= 3697; i++) {
-        const option = document.createElement("option");
-    option.value = i;
-    option.text = i;
-    awardOptions.add(option);
-    }
-}
+		// eslint-disable-next-line no-shadow
+		function parseDataPellGrant( urlSpreadsheet, courseData ) {
+			Papa.parse( urlSpreadsheet, {
+				download: true,
+				header: true,
+				dynamicTyping: true, //convert string to numbers
+				complete( results ) {
+					courseData( results );
+				},
+			}
+			);
+		}
+	}
 
+	function dropDownsPellGrant( data ) {
+		//Populate dropdowns
+		const coursesList = data.data.map( ( { Course } ) => Course );
+		//const csvData = data.data;
+		const dropDownIds = [ 'courseNumber', 'courseNumber2', 'courseNumber3', 'courseNumber4' ];
+
+		let options = '';
+		const selectOptions = '<option disabled selected value>Select Courses</option>';
+		coursesList.map( ( op, i ) => {
+			return options += `<option value="${ op }" id="${ i }">${ op }</option>`;
+		}, {} );
+		for ( let i = 0; i < dropDownIds.length; i++ ) {
+			document.getElementById( dropDownIds[ i ] ).innerHTML = selectOptions + options;
+		}
+
+		//Populate other dropdowns
+		creditDropdown();
+		awardDropdown();
+
+		//functionality to add another course
+		document.getElementById( 'addCourse' ).addEventListener( 'click', addCoursesPellGrant);
+	
+	}
+
+	function creditDropdown() {
+		const creditOptions = document.getElementById( 'c-amount' );
+
+		for ( let i = 1; i <= 12; i++ ) {
+			const option = document.createElement( 'option' );
+			option.value = i;
+			option.text = i;
+			creditOptions.add( option );
+		}
+	}
+
+	function awardDropdown() {
+		const awardOptions = document.getElementById( 'p-award' );
+
+		for ( let i = 370; i <= 3697; i++ ) {
+			const option = document.createElement( 'option' );
+			option.value = i;
+			option.text = i;
+			awardOptions.add( option );
+		}
+	}
+
+	function addCoursesPellGrant() {
+		if ( document.getElementById( 'courseoption3' ).style.display === 'block' ) {
+			document.getElementById( 'courseoption4' ).style.display = 'block';
+			document.getElementById( 'addCourse' ).style.display = 'none';
+		}
+		if ( document.getElementById( 'courseoption2' ).style.display === 'block' ) {
+			document.getElementById( 'courseoption3' ).style.display = 'block';
+		} else {
+			document.getElementById( 'courseoption2' ).style.display = 'block';
+		}
+	}
 }() );
