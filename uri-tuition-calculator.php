019d5bccff7d0e40c240f5159d716334f4fdaffa
alexandragauss <alexandra_gauss@uri.edu>
2025-04-29 12:49:17 -0400

    Bump version

--- uri-tuition-calculator.php
+++ uri-tuition-calculator.php
@@ -3,7 +3,7 @@
  * Plugin Name: URI Tuition Calculator
  * Plugin URI: http://www.uri.edu
  * Description: A calculator for course tuition and fees
- * Version: 1.4.2
+ * Version: 1.5.0
  * Author: URI Web Communications
  * Author URI: https://www.uri.edu/
  *
